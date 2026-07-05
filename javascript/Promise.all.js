1b6ac9f4777c69fdeebdda8a4a7c3b1282c6f200
Priyadarshini Nadar <priyadarshinin.tech@gmail.com>
2026-07-05 14:06:21 +0530

    Implement custom Promise.all function

---
+++ javascript/Promise.all.js
@@ -0,0 +1,24 @@
+function promiseAll(promises) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let completed = 0;
+
+    if (promises.length === 0) {
+      resolve([]);
+      return;
+    }
+
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((value) => {
+          results[index] = value;
+          completed++;
+
+          if (completed === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch(reject);
+    });
+  });
+}
